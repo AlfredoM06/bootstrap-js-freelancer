29e59e5029063496e946fe955fc394c2bc7984c6
AlfredoM06 <Alfredomanfredi06@gmail.com>
2023-11-27 10:21:39 +0100

    started creating the work price  calculation

--- js/main.js
+++ js/main.js
@@ -11,3 +11,25 @@ let email = document.getElementById("email").value;
 let hours = document.getElementById("hours").value;
 let inputTypeWork = document.getElementById("inputTyperWork").value;
 let discountCode = document.getElementById("discount_code").value;
+
+
+// mi creo la base del calcolo per la tariffa oraria 
+
+let backendPrice = 20.50;
+let frontendPrice = 15.30;
+let projectgAnalysisPrice = 33.60;
+let workPrice;
+
+// converto le ore inserire dall'utente in valore numerico per poi fare il calcolo della tariffa
+
+hours = parseInt(hours);
+
+// faccio si che alla scelta dell'utente corrisponda ad un calcolo del prezzo
+
+if (inputTypeWork == 1) {
+    workPrice = backendPrice * hours;
+} else if (inputTypeWork == 2) {
+    workPrice = frontendPrice * hours;
+} else if (inputTypeWork == 3) {
+    workPrice = projectgAnalysisPrice * hù;
+}
