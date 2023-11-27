4ca118567afa7a10c100264e5c8cb94305e0161b
AlfredoM06 <Alfredomanfredi06@gmail.com>
2023-11-27 10:09:28 +0100

    started creating di user input in js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//devo continuare da dove mi sono fermato con l'altro test, devo inserire dei prezzi in base alle ore richieste devo fare 
+// in modo che al click del send si compili il form e si calcoli il prezzo con i dati inseriti e dare la possibilità di applicare 
+// sconti 
+
+
+// inizio collegando le informazione da inserire nel form su html a js 
+
+let name = document.getElementById("name").value;
+let surname = document.getElementById("surname").value;
+let email = document.getElementById("email").value;
+let hours = document.getElementById("hours").value;
+let inputTypeWork = document.getElementById("inputTyperWork").value;
+let discountCode = document.getElementById("discount_code").value;
